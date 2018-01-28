fa119f25f4da7d54d30d0e8290a4c20eee2b4dab
Hidderqu <quentin.hidderley@ensta-bretagne.org>
2018-01-28 18:13:37 +0100

    Added 'New node' Request & clean shutdown

--- Blockchain/client.py
+++ Blockchain/client.py
@@ -2,18 +2,49 @@ import socket
 import sys
 
 
-SK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-IPServ = '127.0.0.1'
-PortServ = 4242
-BuffSize = 128
+def linkToNode(nodeIP, request):
+	'''Connects to Node at nodeIP'''
+	SK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	IPServ = nodeIP
+	PortServ = 4242
+	BuffSize = 128
+
+	SK.connect((IPServ, PortServ))
+	
+	request(SK)
+
+	#data = SK.recv(BuffSize) #Read server echo
+	#print("Server >>>", bytes.decode(data))
+
+	SK.shutdown(socket.SHUT_RDWR)
+	SK.close()
+
+	print("End of connection")
 
 
-SK.connect((IPServ, PortServ))
-SK.send(str.encode(sys.argv[1]))
-data = SK.recv(BuffSize)
 
-SK.shutdown(socket.SHUT_RDWR)
-SK.close()
+def newReq(SK):
+	print("Making 1st contact")
+	SK.send(str.encode("NODE NEW")) #New node declaration
 
-print("Echo >>>", bytes.decode(data))
+def consReq(SK):
+	print("Asking for consensus")	
+	SK.send(str.encode("NODE CONSENSUS")) #Consensus request
+	
+def memReq(SK):
+	print("Offering mempool update")
+	SK.send(str.encode("NODE MEMUP")) #Mempool update request
+	SK.send(str.encode("PyObject0"))
+
+def shutNode(SK):
+	print("Shutting down the other node")
+	SK.send(str.encode("NODE SHUTDOWN"))
+
+
+
+if __name__ == "__main__":
+	
+	linkToNode('127.0.0.1', newReq)
+	linkToNode('127.0.0.1', shutNode)
--- Blockchain/server.py
+++ Blockchain/server.py
@@ -1,33 +1,74 @@
 # coding=utf-8
 
 import socket
+import sys
 
-IPServ = "localhost"
-PortServ = 4242
 BuffSize = 32
 QueuedCo = 3
+Nodes = set([]) #set instead of list to avoid duplicates
 
-'''Listening Socket'''
-SKL = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SKL.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-SKL.bind((IPServ, PortServ))
-SKL.listen(QueuedCo)
+def startNode():
+	'''Start a server on Node'''
+	IPServ = '0.0.0.0'
+	PortServ = 4242
 
-'''Serving socket'''
-(SKS, Add) = SKL.accept()
-print("Connection established with", Add)
+	'''Init Listening Socket'''
+	SKL = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	SKL.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	SKL.bind((IPServ, PortServ))
+	SKL.listen(QueuedCo)
 
-while(True):
-	data = SKS.recv(BuffSize)
-	if not data:
-		print("No more data, end of connection with", Add)
-		break
-	print(">>>", bytes.decode(data))
-	SKS.send(data) #Echo ON
+	print("[OK] Node initialized")
+
+	return SKL
+
+def serveNode(listenSock):
+	'''Start a service for incoming connections on listenSock'''
+	while (True):
+		print("Waiting for incoming connection...")
+		(SKS, Add) = listenSock.accept()
+		
+		data = bytes.decode(SKS.recv(BuffSize))
+		ReqType, Req = data.split(' ')[0], data.split(' ')[1] #Splitting request
+
+		if ((Add[0] not in Nodes) & (Req != "NEW")):
+			print("Unkown node attempting to connect - Refusing")
+			SKS.send(str.encode("Unknown Node, please send NODE NEW request first"))  #Refusing unknown nodes
+
+		else:
+			print("Connection established with %s on port %d" %(Add[0], Add[1]))
+			
+			if ReqType == "NODE":
+				print("Node request")
+				if Req == "SHUTDOWN":
+					SKS.shutdown(socket.SHUT_RDWR)
+					SKS.close()
+					shutNode(listenSock)
+				if Req == "NEW":
+					print("New node reaching out...")
+					Nodes.add(Add[0])
+					print(Nodes)
+
+			while (data):
+				print("Client >>>", data)
+				SKS.send(str.encode(data)) #Echo to client
+				data = SKS.recv(BuffSize)
+		
+		SKS.shutdown(socket.SHUT_RDWR)
+		SKS.close()
+		print("Ended connection with %s" %(Add[0]))
 
 
-SKS.shutdown(socket.SHUT_RDWR)
-SKS.close()
-SKL.shutdown(socket.SHUT_RDWR)
-SKL.close()
+def shutNode(listenSock):
+	'''Shutdown & Close all sockets, close node'''
+	print("Shutting down node...")
+	listenSock.shutdown(socket.SHUT_RDWR)
+	listenSock.close()
+	print("[OK] Node shut down")
+	sys.exit()
+
+
+if __name__ == "__main__":
+	SKL = startNode()
+	serveNode(SKL)
 
